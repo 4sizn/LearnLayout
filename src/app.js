dc2d19b3d29e74c63f4369a00d1ed580d5a9248f
hsshin <hsshin@hsshinui-MacBook-Pro.local>
2018-01-03 16:05:47 +0900

    init

---
+++ src/app.js
@@ -0,0 +1,68 @@
+function fixed() {
+    document.getElementById('wrap').className = 'fixed';
+}
+
+function liquid() {
+    document.getElementById('wrap').className = 'liquid';
+}
+
+function hybrid() {
+    document.getElementById('wrap').className = 'hybrid';
+}
+
+function c() {
+    document.getElementById('container').className = 'c';
+    document.getElementsByClassName('pre1iew_img')[0].src = 'images/c.gif';
+}
+
+function ce() {
+    document.getElementById('container').className = 'ce';
+    document.getElementsByClassName('preview_img')[0].src = 'images/ce.gif';
+}
+
+function ec() {
+    document.getElementsByClassName('preview_img')[0].src = 'images/ec.gif';
+    document.getElementById('container').className = 'ec';
+
+}
+
+function cee() {
+    document.getElementsByClassName('preview_img')[0].src = 'images/cee.gif';
+    document.getElementById('container').className = 'cee';
+
+}
+
+function ece() {
+    document.getElementsByClassName('preview_img')[0].src = 'images/ece.gif';
+    document.getElementById('container').className = 'ece';
+
+}
+
+function eec() {
+    document.getElementsByClassName('preview_img')[0].src = 'images/eec.gif';
+    document.getElementById('container').className = 'eec';
+}
+
+function imgC(){
+    document.getElementsByClassName('preview_img')[0].src = 'images/c.gif';
+}
+
+function imgCE(){
+    document.getElementsByClassName('preview_img')[0].src = 'images/ce.gif';
+}
+
+function imgEC(){
+    document.getElementsByClassName('preview_img')[0].src = 'images/ec.gif';
+}
+
+function imgCEE(){
+    document.getElementsByClassName('preview_img')[0].src = 'images/cee.gif';
+}
+
+function imgECE(){
+    document.getElementsByClassName('preview_img')[0].src = 'images/ece.gif';
+}
+
+function imgEEC(){
+    document.getElementsByClassName('preview_img')[0].src = 'images/eec.gif';
+}
