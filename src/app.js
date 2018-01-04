58cfdab965bcb326bc49feb337015ef7784eb6b5
hsshin <hsshin@hsshinui-MacBook-Pro.local>
2018-01-04 10:59:08 +0900

    pure javascript wok_1 layout design done set tag user-data attr

--- src/app.js
+++ src/app.js
@@ -1,68 +1,26 @@
-function fixed() {
-    document.getElementById('wrap').className = 'fixed';
+//$(document).ready(){} transfer  IE +9;
+document.addEventListener("DOMContentLoaded", function(event){
+    domReady();
+});
+
+function domReady(){
+    document.querySelector('.layout_lst').addEventListener('click', function(e){
+        changeLayout(e);
+    });
 }
 
-function liquid() {
-    document.getElementById('wrap').className = 'liquid';
+//Initial Parameter Not Support in IE, Opera, Safari...
+/*function changeImg(e, imgsrc = 'preview_img'){
+// document.getElementsByClassName(imgsrc)[0].src = 'images/' + e.target.getAttribute('data-con') + '.gif';
+}*/
+
+function changeImg(e){
+ document.getElementsByClassName('preview_img')[0].src = 'images/' + e.target.getAttribute('data-con') + '.gif';
 }
 
-function hybrid() {
-    document.getElementById('wrap').className = 'hybrid';
-}
 
-function c() {
-    document.getElementById('container').className = 'c';
-    document.getElementsByClassName('pre1iew_img')[0].src = 'images/c.gif';
-}
-
-function ce() {
-    document.getElementById('container').className = 'ce';
-    document.getElementsByClassName('preview_img')[0].src = 'images/ce.gif';
-}
-
-function ec() {
-    document.getElementsByClassName('preview_img')[0].src = 'images/ec.gif';
-    document.getElementById('container').className = 'ec';
-
-}
-
-function cee() {
-    document.getElementsByClassName('preview_img')[0].src = 'images/cee.gif';
-    document.getElementById('container').className = 'cee';
-
-}
-
-function ece() {
-    document.getElementsByClassName('preview_img')[0].src = 'images/ece.gif';
-    document.getElementById('container').className = 'ece';
-
-}
-
-function eec() {
-    document.getElementsByClassName('preview_img')[0].src = 'images/eec.gif';
-    document.getElementById('container').className = 'eec';
-}
-
-function imgC(){
-    document.getElementsByClassName('preview_img')[0].src = 'images/c.gif';
-}
-
-function imgCE(){
-    document.getElementsByClassName('preview_img')[0].src = 'images/ce.gif';
-}
-
-function imgEC(){
-    document.getElementsByClassName('preview_img')[0].src = 'images/ec.gif';
-}
-
-function imgCEE(){
-    document.getElementsByClassName('preview_img')[0].src = 'images/cee.gif';
-}
-
-function imgECE(){
-    document.getElementsByClassName('preview_img')[0].src = 'images/ece.gif';
-}
-
-function imgEEC(){
-    document.getElementsByClassName('preview_img')[0].src = 'images/eec.gif';
+function changeLayout(e){
+ document.getElementById('wrap').className = e.target.getAttribute('data-layout');   
+    document.getElementById('container').className = e.target.getAttribute('data-con');
+    changeImg(e);
 }
