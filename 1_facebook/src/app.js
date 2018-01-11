0297dedf1249f3498b45f732090dd62a28be8b85
hsshin <hsshin@hsshinui-MacBook-Pro.local>
2018-01-11 17:15:47 +0900

    img/sub_type add
    click event update

--- 1_facebook/src/app.js
+++ 1_facebook/src/app.js
@@ -5,13 +5,40 @@ $(document).ready(function(){
     $('#loginFrm').submit(function(){
         alert("submit");
     });
-    $('input').click(function(){
-        alert(this);
+
+    $('#regFrm').submit(function(){
+        alert("submit()");
+        validationForm();
+    });
+
+    $('input').focusin(function(){
+        if(this.value === ''){
+            validationTooltip(this.id, true);
+        }
+    }).focusout(function(){
+        if(this.value === ''){
+            $(this).attr('data-invalid', true);
+        }
+        validationTooltip(this.id, false);
     });
 });
 
-function validationForm(){
+function validationTooltip(id, flag){
+if(id === 'undefined'){
+    console.log(Function.name + 'is undefunded');
+    return;
+}
+    $('.content_layer_pointer').each(function(){
+        if($(this).attr('data-target') === id)
+        {
+            console.log("validation tooltip"+id);
+            $(this).attr('data-focus', flag);
+        }
+    });
+}
 
+function validationForm(){
+    alert($('#loginFrm > [data-invalid]'));
 }
 
 function foo(){
@@ -19,7 +46,7 @@ function foo(){
 }
 
 
-//포커싱 후 벨리데이션 체크
+//포커싱 후 벨리데 체크
 //포커싱 후 널값이면 attr-invalid=true 체크
 //널값이 아니면 사용자 속성값 없어짐
 
