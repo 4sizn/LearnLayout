56f8de14c99ab1b72d669deec1a7d3f642eb7073
hsshin <hsshin@hsshinui-MacBook-Pro.local>
2018-01-12 15:26:48 +0900

    css update

--- 1_facebook/src/app.js
+++ 1_facebook/src/app.js
@@ -3,21 +3,19 @@
 //dom on ready
 $(document).ready(function(){
     $('#loginFrm').submit(function(){
-        alert("submit");
-        validationForm();
+        return validationForm();
     });
 
     $('#regFrm').submit(function(){
-        alert("submit()");
-        validationForm();
+         return validationForm(()=>validationRadio());
     });
-
+    
     $('input').focusin(function(){
-        if(this.value.length == 0){
+        if(this.value == ''){
             validationTooltip(this.id, true);
         }
     }).focusout(function(){
-        if(this.value.length == 0){
+        if(this.value == ''){
             $(this).attr('data-invalid', true);
         }
         else{
@@ -35,26 +33,47 @@ if(id === 'undefined'){
     $('.content_layer_pointer').each(function(){
         if($(this).attr('data-target') === id)
         {
-            console.log("validation tooltip"+id);
+            console.log("validation tooltip" + id);
             $(this).attr('data-focus', flag);
         }
     });
 }
 
 function validationForm(){
-    if($(this).parent == $('#loginFrm')){
-        
-    }
-    else if($(this).parent == $('#regFrm')){
-    }
+    return validateRadio();
+        //성
+        //이름
+        //휴대폰 번호 또는 이메일
+        //새비밀번호
+        //생일
+        //성
+        //---아니면 Validate-data 변경---
+    return false;
 }
 
+function validateRadio(){
+    var radios = $(this).children('input[type=radio]');
+    console.log(radios);
 
-function foo(){
-    alert("fopo");
+    return true;
 }
 
+function validateEmail(inputText)  
+{  
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
 
+            if(inputText.value.match(mailformat))  
+            {  
+                document.form1.text1.focus();  
+                return true;  
+            }  
+            else  
+            {  
+                alert("You have entered an invalid email address!");  
+                document.form1.text1.focus();  
+                return false;  
+            }  
+}  
 //포커싱 후 벨리데 체크
 //포커싱 후 널값이면 attr-invalid=true 체크
 //널값이 아니면 사용자 속성값 없어짐
